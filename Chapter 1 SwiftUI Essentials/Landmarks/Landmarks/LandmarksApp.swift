45dbb017271b8de580267d6698c402bc0744f18e
Chanchal Raj (CORP HO) <chraj@mafcarrefour.com>
2024-11-21 23:52:21 +0400

    Created first Landmark project

---
+++ Chapter 1 SwiftUI Essentials/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Chanchal Raj on 21/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
